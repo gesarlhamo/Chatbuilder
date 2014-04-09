ea9dae751cba0156a3299e0ad7d076a7290c3fdf
Gesang <gesarlhamo80@gmail.com>
2014-04-09 11:52:11 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chatbuilder::Application.config.secret_key_base = '2dfbe5c55718d0d7e888d81fde12d840e8f671315398e8cbbed4b251f13e646721bef0a04f11c19c5863fdd26e4209da625a7201e3078e6d70ce140bdf5bf718'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chatbuilder::Application.config.session_store :cookie_store, key: '_chatbuilder_session'
